bcca119b1bd71d295ec309768fb04dfeff2c396e
Davi Queiroz <78423321+davibrandao@users.noreply.github.com>
2023-08-29 01:35:22 -0300

    Added mouse drag painting functionality and updated container design

--- script.js
+++ script.js
@@ -10,9 +10,22 @@ function createGrid(size) {
 
 createGrid(16);
 const squares = document.querySelectorAll('.square');
+let isPainting = false; // Variável para controlar se estamos pintando
+
 squares.forEach(square => {
-    square.addEventListener('click', () => {
-        square.style.backgroundColor = '#000'; // Altere a cor ao passar o mouse
+    square.addEventListener('mousedown', () => {
+        isPainting = true; // Começar a pintura quando o botão é pressionado
+        square.style.backgroundColor = '#000'; // Pinte o quadrado imediatamente
+    });
+
+    square.addEventListener('mouseup', () => {
+        isPainting = false; // Parar a pintura quando o botão é liberado
+    });
+
+    square.addEventListener('mouseenter', () => {
+        if (isPainting) {
+            square.style.backgroundColor = '#000'; // Pintar enquanto o mouse estiver pressionado
+        }
     });
 });
 
